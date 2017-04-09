27f27fdc5a42735ca09b703515385db064c16e48
Everett Hildenbrandt <hildenb2@illinois.edu>
2017-04-09 18:59:34 -0500

    saving work

--- adts/adts.hs
+++ adts/adts.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ViewPatterns #-}
 
 
 --- Algebraic Structure
@@ -26,12 +28,22 @@ instance (Eq d, Eq (n (Term n d))) => Eq (Term n d) where
     Node n == Node n' = n == n'
     _      == _       = False
 
-class Functor n => Reduce n d where
-    reduce :: Term n d -> Term n d
+class Reifiable n d where
+    reify :: (Reducable n) => d -> Term n d
+    --- unreify :: (Reducable n) => Term n d -> Term n d
+
+class (Functor n) => Reducable n where
+    reduce :: n (Term n d) -> Term n d
 
     reduceAll :: Term n d -> Term n d
-    reduceAll (Node n) = reduce $ Node (fmap reduceAll n)
-    reduceAll d        = reduce d
+    reduceAll (Node n) = reduce $ fmap reduceAll n
+    reduceAll (Data d) = Data d
+
+    --- Law (idempotence):
+    --- reduce . reduce == reduce
+
+class Modelable n d where
+    model :: Term n d -> Term n d
 
 
 --- Data
@@ -90,54 +102,75 @@ data SNat a = Z
             deriving (Show, Eq)
 
 instance Functor SNat where
-    fmap f (Z)   = Z
-    fmap f (S a) = S (f a)
+    fmap f (Z)     = Z
+    fmap f (S a)   = S (f a)
+    fmap f (P a b) = P (f a) (f b)
+    fmap f (T a b) = T (f a) (f b)
 
-type SNatInt = Term SNat IntData
+--- instance Reducable SNat where
 
-z :: SNatInt
+type SNatInt = Term SNat
+
+z :: SNatInt d
 z = Node Z
---- alternatively z = int 0
 
-s :: SNatInt -> SNatInt
+s :: SNatInt d -> SNatInt d
 s n = Node (S n)
 
-pn :: Integer -> SNatInt
-pn 0         = z
-pn n | n > 0 = s $ pn (n - 1)
+p :: SNatInt d -> SNatInt d -> SNatInt d
+p a b = Node (P a b)
 
-p :: SNatInt -> SNatInt -> SNatInt
-p (Node (S x)) y = Node (S (p x y))
-p (Node Z)     y = y
+t :: SNatInt d -> SNatInt d -> SNatInt d
+t a b = Node (T a b)
 
-t :: SNatInt -> SNatInt -> SNatInt
-t (Node (S x)) y = p (t x y) y
-t (Node Z)     y = z
+snat :: Integer -> SNatInt d
+snat 0         = z
+snat n | n > 0 = s $ snat (n - 1)
 
-instance Reduce SNat IntData where
-    reduce (Node Z)       = z
-    reduce (Node (S a))   = s a
-    reduce (Node (P a b)) = p a b
-    reduce (Node (T a b)) = t a b
+--- vars a b .
+---
+--- eq P Z     b = b .
+--- eq P (S a) b = S (P a b) .
+---
+--- eq T Z     b = Z .
+--- eq T (S a) b = P b (T a b) .
+
+instance Reducable SNat where
+    reduce (P (Node Z) b)     = b
+    reduce (P (Node (S a)) b) = Node (S (reduce $ P a b))
+    reduce (T (Node Z) b)     = Node Z
+    reduce (T (Node (S a)) b) = reduce $ P (reduce $ T a b) b
+    reduce n                  = Node n
+
+instance Reifiable SNat IntData where
+    reify (I 0)         = Node Z
+    reify (I n) | n > 0 = Node (S (reify (I $ n - 1)))
+    reify (V v)         = var v
+
+instance Modelable SNat IntData where
+    model (Node Z)     = int 0
+    model (Node (S a)) = case model a of
+                             Data (I i) -> int (i + 1)
+                             Data (V v) -> var v
 
 
 --- A free Ring over `IntData`
 --- --------------------------
 
 
-data Ring a = Neg   a
-            | Plus  a a
-            | Times a a
-            | Zero
+data Ring a = Zero
             | One
+            | Neg a
+            | Plus a a
+            | Times a a
             deriving (Show, Eq)
 
 instance Functor Ring where
-    fmap f (Plus  a b) = Plus  (f a) (f b)
-    fmap f (Neg a)     = Neg (f a)
-    fmap f (Times a b) = Times (f a) (f b)
     fmap f (Zero)      = Zero
     fmap f (One)       = One
+    fmap f (Neg a)     = Neg (f a)
+    fmap f (Plus  a b) = Plus (f a) (f b)
+    fmap f (Times a b) = Times (f a) (f b)
 
 
 --- Here are some "smart constructors" for the `Ring` data-structure, that make it
@@ -149,51 +182,70 @@ instance Functor Ring where
 --- Negatives propagate inward.
 
 
-type RingInt = Term Ring IntData
+type RingInt = Term Ring
 
-zero :: RingInt
-zero = int 0
+zero :: RingInt d
+zero = Node Zero
 
-one :: RingInt
-one = int 1
+one :: RingInt d
+one = Node One
 
-neg :: RingInt -> RingInt
-neg (Data (I i))       = Data (I (- i))
-neg (Data (V i))       = Node $ Neg (Data (V i))
-neg (Node (Plus a b))  = Node $ Plus (neg a) (neg b)
-neg (Node (Times a b)) = Node $ Times (neg a) b
-neg a                  = Node $ Neg a
+neg :: RingInt d -> RingInt d
+neg = Node . Neg
 
-plus :: RingInt -> RingInt -> RingInt
-plus (Node Zero)         b                 = b
-plus a                   (Node Zero)       = plus zero a
-plus (Node One)          (Data (I i))      = int (i + 1)
-plus a                   (Node One)        = plus one a
-plus (Data (I i1))       (Data (I i2))     = int (i1 + i2)
-plus a@(Data (V _))      (Node (Plus b c)) = plus b (plus a c)
-plus a                   (Node (Plus b c)) = plus (plus a b) c
---- plus p@(Node (Plus _ _)) b                 = plus b p
-plus a                   b                 = Node $ Plus a b
+plus :: RingInt d -> RingInt d -> RingInt d
+plus a b = Node $ Plus a b
 
-times :: RingInt -> RingInt -> RingInt
-times (Node Zero)          b                  = zero
-times a                    (Node Zero)        = times zero a
-times (Node One)           b                  = b
-times a                    (Node One)         = a
-times (Data (I i1))        (Data (I i2))      = int (i1 * i2)
-times a@(Data (V _))       (Node (Times b c)) = times b (times a c)
-times a                    (Node (Times b c)) = times (times a b) c
-times t@(Node (Times _ _)) b                  = times b t
-times a                    (Node (Plus b c))  = plus (times a b) (times a c)
-times a                    (Node (Neg b))     = times (neg a) b
-times a                    b                  = Node $ Times a b
+times :: RingInt d -> RingInt d -> RingInt d
+times a b = Node $ Times a b
 
-instance Reduce Ring IntData where
-    reduce (Node Zero)        = zero
-    reduce (Node One)         = one
-    reduce (Node (Neg a))     = neg a
-    reduce (Node (Plus a b))  = plus a b
-    reduce (Node (Times a b)) = times a b
+ring :: Int -> RingInt d
+ring 0         = zero
+ring n | n > 0 = plus (ring (n - 1)) one
+
+--- vars a b .
+---
+--- eq Neg (Neg a)    = a .
+--- eq Neg (Plus a b) = Plus (Neg a) (Neg b) .
+---
+--- eq Plus a Zero = a .
+--- eq Plus Zero b = b .
+---
+--- eq Times (Neg a) b    = Neg (Times a b) .
+--- eq Times a (Neg b)    = Neg (Times a b) .
+--- eq Times a (Plus b c) = Plus (Times a b) (Times a c) .
+--- eq Times (Plus a b) c = Plus (Times a c) (Times b c) .
+--- eq Times a One        = a .
+--- eq Times One b        = b .
+--- eq Times a Zero       = Zero .
+--- eq Times Zero b       = Zero .
+
+add1 :: RingInt d -> RingInt d
+add1 (reduceAll -> Node (Neg a)) = Node $ Plus (Node (Neg a)) (Node One)
+
+instance Reducable Ring where
+    reduce (Neg (Node (Neg a)))    = a
+    reduce (Neg (Node (Plus a b))) = let na = reduce $ Neg a
+                                         nb = reduce $ Neg b
+                                     in  reduce $ Plus na nb
+
+    reduce (Plus a (Node Zero)) = a
+    reduce (Plus (Node Zero) b) = b
+
+    reduce (Times (Node (Neg a)) b)    = reduce $ Neg (reduce $ Times a b)
+    reduce (Times a (Node (Neg b)))    = reduce $ Neg (reduce $ Times a b)
+    reduce (Times a (Node (Plus b c))) = let ab = reduce $ Times a b
+                                             ac = reduce $ Times a c
+                                         in  reduce $ Plus ab ac
+    reduce (Times (Node (Plus a b)) c) = let ac = reduce $ Times a c
+                                             bc = reduce $ Times b c
+                                         in  reduce $ Plus ac bc
+    reduce (Times a (Node One))  = a
+    reduce (Times (Node One) b)  = b
+    reduce (Times a (Node Zero)) = Node Zero
+    reduce (Times (Node Zero) b) = Node Zero
+
+    reduce n = Node n
 
 
 --- The `if_then_else_` construct
@@ -215,13 +267,13 @@ instance Functor ITE where
 
 data Set a = Set a
            | MtSet
-           | Union a a
+           | a :+ a
            deriving (Show, Eq)
 
 instance Functor Set where
-    fmap f (Set a)     = Set (f a)
-    fmap f (MtSet)     = MtSet
-    fmap f (Union a b) = Union (f a) (f b)
+    fmap f (Set a)  = Set (f a)
+    fmap f (MtSet)  = MtSet
+    fmap f (a :+ b) = (f a) :+ (f b)
 
 type SetInt = Term Set IntData
 
@@ -230,14 +282,14 @@ mt = Node MtSet
 
 set :: Int -> SetInt
 set n = Node (Set (Data (I n)))
-
-union :: SetInt -> SetInt -> SetInt
-union (Node MtSet) b                      = b                   --- MtSet is left-id
-union a (Node MtSet)                      = a                   --- MtSet is right-id
-union a b | a == b                        = a                   --- idempotence
-union (Node (Union a b)) c                = union a (union b c) --- idempotence
-union a (Node (Union b c)) = let ab = union a b
-                             in  if ab == b then Node $ Union b c else union ab c
---- union a@(Node (Set _)) (Node (Union b c)) = union b (union a c) --- idempotence
-union a@(Node (Set _)) b@(Node (Set _))   = Node (Union a b)    --- base case
+--- 
+--- union :: SetInt -> SetInt -> SetInt
+--- union (Node MtSet) b                      = b                   --- MtSet is left-id
+--- union a (Node MtSet)                      = a                   --- MtSet is right-id
+--- union a b | a == b                        = a                   --- idempotence
+--- union (Node (Union a b)) c                = union a (union b c) --- idempotence
+--- union a (Node (Union b c)) = let ab = union a b
+---                              in  if ab == b then Node $ Union b c else union ab c
+--- --- union a@(Node (Set _)) (Node (Union b c)) = union b (union a c) --- idempotence
+--- union a@(Node (Set _)) b@(Node (Set _))   = Node (Union a b)    --- base case
 
